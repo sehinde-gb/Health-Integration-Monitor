6efa75749929a40945866a8b92a75d1ca90b47e2
Sehinde Raji <profusity@gmail.com>
2026-06-11 13:08:13 +0100

    updated post in presentational child component

--- src/app/features/posts/components/post-list-table/post-list-table.component.ts
+++ src/app/features/posts/components/post-list-table/post-list-table.component.ts
@@ -1,18 +1,15 @@
 import { CommonModule } from "@angular/common";
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Component, input, output } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { Post } from "../../models/post";
 
-
 @Component({
-    selector: 'app-post-list-table',
-    imports: [CommonModule, RouterModule],
-    templateUrl: './post-list-table.component.html',
-    styleUrl: './post-list-table.component.css'
+  selector: 'app-post-list-table',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './post-list-table.component.html',
+  styleUrl: './post-list-table.component.css'
 })
 export class PostListTableComponent {
-  // Inputs (data)
-  @Input() posts: Post[] = [];
-  @Output() deletePost = new EventEmitter<number>();
-
+  posts = input<Post[]>([]);
+  deletePost = output<number>();
 }
